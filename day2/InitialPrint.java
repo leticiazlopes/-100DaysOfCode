ccb8f00fa82e66d25eead1693ff8ec0940edc575
Leticia Lopes Lima <leticiazlzlima@gmail.com>
2024-12-16 20:40:01 -0300

    solution

---
+++ day2/InitialPrint.java
@@ -0,0 +1,16 @@
+public class InitialPrint {
+    public static void main(String[] args) {
+        //Day 2 - Variables and Data Types
+        //1. Declare and initialize variables for various data types including 
+        //integer (int), floating-point number (double), and string (String). 
+        //Then, print each variable to the console.
+
+        int age = 21;
+        double salary = 2500.50;
+        String name = "Leticia";
+
+        System.out.println("Nome: " + name + ", Idade: " + age +  ", Salário: " + salary);
+
+
+    }
+}
